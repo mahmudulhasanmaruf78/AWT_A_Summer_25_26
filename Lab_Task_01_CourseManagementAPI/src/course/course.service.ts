39bd3f99041f070b33215e6a4bf7bafe6fe3020b
mahmudulhasanmaruf78@gmail.com <mahmudulhasanmaruf78@gmail.com>
2026-07-02 10:50:19 +0600

    feat: implement CourseService with CRUD methods for course management

--- Lab_Task_01_CourseManagementAPI/src/course/course.service.ts
+++ Lab_Task_01_CourseManagementAPI/src/course/course.service.ts
@@ -1,4 +1,23 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class CourseService {}
+export class CourseService {
+  getAllCourses(): string {
+    return 'Get All Courses - from Service';
+  }
+  getCourseById(id: number): string {
+    return `Get Course by ID: ${id} - from Service`;
+  }
+  createCourse(course: any): string {
+    return 'Create Course - from Service';
+  }
+  updateCourse(id: number, course: any): string {
+    return `Update Course ${id} - from Service`;
+  }
+  patchCourse(id: number, course: any): string {
+    return `Patch Course ${id} - from Service`;
+  }
+  deleteCourse(id: number): string {
+    return `Delete Course ${id} - from Service`;
+  }
+}
