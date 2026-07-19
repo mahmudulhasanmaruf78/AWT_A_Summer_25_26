df7d6b6dfcaddee764969aadb4157009317eb945
mahmudulhasanmaruf78@gmail.com <mahmudulhasanmaruf78@gmail.com>
2026-07-19 09:14:57 +0600

    feat: add TypeOrmModule import for Products entity in ProductsModule

--- product-inventory-api/src/products/products.module.ts
+++ product-inventory-api/src/products/products.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Products } from './entities/products.entity';
 import { ProductsController } from './products.controller';
 import { ProductsService } from './products.service';
-import { ProductsController } from './products.controller';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Products])],
   controllers: [ProductsController],
   providers: [ProductsService]
 })
